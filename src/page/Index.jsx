a87788b127a0f4b3c6c2153fab8f38b902f4daa3
Woneeee <a6885088@naver.com>
2025-04-02 15:59:01 +0900

    코드 정리

--- src/page/Index.jsx
+++ src/page/Index.jsx
@@ -13,6 +13,8 @@ const Main = ({ Cur_Sec, setCur_Sec }) => {
         setCur_Sec((prev) => Math.max(prev - 1, 0));
       }
     }, 300);
+    // Debounce 는 특정 함수가 일정시간안에 여러번 호출되더라도, 마지막 호출만 처리하도록 만들어줌
+    // 이 코드에서는 스크롤 이벤트가 너무 자주 발생하는 것을 방지하고, 사용자가 스크롤을 멈춘 후 일정 시간 지나면 한번만 실행
 
     // 키보드
     const handleKeyDown = Debounce((e) => {
@@ -25,22 +27,30 @@ const Main = ({ Cur_Sec, setCur_Sec }) => {
 
     window.addEventListener("wheel", handleScroll, { passive: true });
     window.addEventListener("keydown", handleKeyDown);
+    // wheel 이벤트 리스너
+    // keydown 이벤트 리스너
 
     return () => {
       window.removeEventListener("wheel", handleScroll);
       window.removeEventListener("keydown", handleKeyDown);
     };
   }, [Cur_Sec, setCur_Sec]);
+  //  의존성 배열
+  // Cur_Sec, setCur_Sec 중 하나라도 변경되면, useEffect 가 실행
+  // 리턴 값은 이벤트 리스너 정리 (불필요한 이벤트 리스너가 남지 않도록 처리)
+  // 먼저, 이전에 등록된 이벤트 리스너를 제거하고, 그 후에 새로운 이벤트 리스너를 등록
 
   useEffect(() => {
     if (Ref_Sec.current[Cur_Sec]) {
       Ref_Sec.current[Cur_Sec].scrollIntoView({ behavior: "smooth" });
+      // element.scrollIntoView(options);
     }
   }, [Cur_Sec]);
 
   const handleNavClick = (idx) => {
     setCur_Sec(idx);
   };
+  // handleNavClick 함수는 네비게이션 클릭 시 현재 섹션을 변경하는 역할
 
   return (
     <>
